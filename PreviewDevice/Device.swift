4351e9d6f4152ff99ffc19979d0450ea3eb64846
Anton Paliakou <toxa95401@gmail.com>
2021-08-23 01:04:36 +0300

    Add mac & apple tv devices description.

--- PreviewDevice/Device.swift
+++ PreviewDevice/Device.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-public enum Device {
+public enum Device: String {
     
+    // MARK: - Mac
+    
+    case mac = "Mac"
+    case macCatalyst = "Mac Catalyst"
+    
+    // MARK: - Apple TV
+    
+    case appleTV = "Apple TV"
+    case appleTV4K = "Apple TV 4K"
+    case appleTV4K1080p = "Apple TV 4K (at 1080p)"
 }
