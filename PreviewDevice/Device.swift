bd3f1762fa5cbcfa1b7ea0448c3bc0389fce1692
Anton Paliakou <toxa95401@gmail.com>
2021-08-23 01:18:20 +0300

    Add apple watches to Device.

--- PreviewDevice/Device.swift
+++ PreviewDevice/Device.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+#if canImport(SwiftUI)
+
 public enum Device: String {
     
     // MARK: - Mac
@@ -19,4 +21,23 @@ public enum Device: String {
     case appleTV = "Apple TV"
     case appleTV4K = "Apple TV 4K"
     case appleTV4K1080p = "Apple TV 4K (at 1080p)"
+    
+    // MARK: - Apple Watch
+    
+    case watch_38mm = "Apple Watch - 38mm"
+    case watch_42mm = "Apple Watch - 42mm"
+    case watchSeries2_38mm = "Apple Watch Series 2 - 38mm"
+    case watchSeries2_42mm = "Apple Watch Series 2 - 42mm"
+    case watchSeries3_38mm = "Apple Watch Series 3 - 38mm"
+    case watchSeries3_42mm = "Apple Watch Series 3 - 42mm"
+    case watchSeries4_40mm = "Apple Watch Series 4 - 40mm"
+    case watchSeries4_44mm = "Apple Watch Series 4 - 44mm"
+    case watchSeries5_40mm = "Apple Watch Series 5 - 40mm"
+    case watchSeries5_44mm = "Apple Watch Series 5 - 44mm"
+    case watchSE_40mm = "Apple Watch SE - 40mm"
+    case watchSE_44mm = "Apple Watch SE - 44mm"
+    case watchSeries6_40mm = "Apple Watch Series 6 - 40mm"
+    case watchSeries6_44mm = "Apple Watch Series 6 - 44mm"
 }
+
+#endif
