ce87b1782c77db7a2275df5ae3f1440ad003270a
Anton Paliakou <toxa95401@gmail.com>
2021-08-25 01:00:21 +0300

    Add available for MacCatalyst.

--- PreviewDevice/View+PreviewDevice.swift
+++ PreviewDevice/View+PreviewDevice.swift
@@ -8,7 +8,7 @@
 #if canImport(SwiftUI)
 import SwiftUI
 
-@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+@available(iOS 13.0, OSX 10.15, macCatalyst 13.0, tvOS 13.0, watchOS 6.0, *)
 public extension View {
     
     func previewDevice(device: Device) -> some View {
